f0e227e9f5c10a897218856eead6cab2824d4728
Nok1on1 <giomiqa2@gmail.com>
2026-04-11 18:03:30 +0400

    Refactor VRTeasy code structure; rename classes for consistency, implement PDF download functionality, and enhance property management.

---
+++ vrteasy-core/src/main/java/ge/tbc/testautomation/data/Constants.java
@@ -0,0 +1,5 @@
+package ge.tbc.testautomation.data;
+
+public class Constants {
+  public static final String FILE_NAME = "vrteasy.properties";
+}
---
+++ vrteasy-core/src/main/java/ge/tbc/testautomation/data/Properties.java
@@ -0,0 +1,25 @@
+package ge.tbc.testautomation.data;
+
+import ge.tbc.testautomation.VRTeasyConfigProvider;
+
+public class Properties {
+
+  private static boolean hasInstance;
+  public static String downloadFolder;
+  public static int downloadTimeout;
+  public static int downloadTick;
+  public static int pdfImageDPI;
+
+  public Properties() {
+    if (hasInstance) {
+      return;
+    }
+
+    var properties = VRTeasyConfigProvider.loadProperties();
+    downloadFolder = properties.getProperty("download.folder", "/downloads");
+    downloadTimeout = Integer.parseInt(properties.getProperty("download.timeout", "3"));
+    downloadTick = Integer.parseInt(properties.getProperty("download.tick", "300"));
+    pdfImageDPI = Integer.parseInt(properties.getProperty("pdf.image.dpi", "300"));
+    hasInstance = true;
+  }
+}
